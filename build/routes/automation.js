c97f9cc0303a6f19906641f2cfbacc0a0bfedc2e
Rahul kumar <rahul.kumar@valuecreatives.com>
2024-09-21 14:57:55 +0530

    changes

--- build/routes/automation.js
+++ build/routes/automation.js
@@ -4,4 +4,5 @@ const express_1 = require("express");
 const router = (0, express_1.Router)();
 const automation_1 = require("../controller/automation");
 router.post("/create", automation_1.createAutomation);
+router.get("/list", automation_1.getAutomation);
 exports.default = router;
--- build/routes/webhook.js
+++ build/routes/webhook.js
@@ -3,5 +3,5 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = require("express");
 const router = (0, express_1.Router)();
 const webhook_1 = require("../controller/webhook");
-router.post("/webhook", webhook_1.webhook);
+router.post("/", webhook_1.webhook);
 exports.default = router;
